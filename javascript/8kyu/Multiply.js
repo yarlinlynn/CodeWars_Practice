4b9e21ea35a9ec9268e56a25c32312158ad42498
Yarlin <yarlinlynn@gmail.com>
2025-08-22 07:14:18 +0200

    soltion for multiply  challenge

---
+++ javascript/8kyu/Multiply.js
@@ -0,0 +1,15 @@
+
+/**
+Instructions:
+This code does not execute properly. Try to figure out why.
+*/
+
+// Solution:
+function multiply(a, b){
+  return a * b
+}
+
+/*
+Best rated solution:
+multiply = (a, b) => a * b;
+*/

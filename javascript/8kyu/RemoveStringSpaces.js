0096f34945919f443494264f2a6c8ac9b6dee454
Yarlin <yarlinlynn@gmail.com>
2025-08-23 12:24:37 +0200

    solution for removing spaces from strings

---
+++ javascript/8kyu/RemoveStringSpaces.js
@@ -0,0 +1,42 @@
+/*
+Instructions:
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples (Input -> Output):
+
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+*/
+
+// Solution:
+const noSpace = x => x.split(" ").join("")
+
+/*
+Best rated solutions:
+
+function noSpace(x){
+  return x.split(" ").join("")
+}
+
+function noSpace(x){
+  return x.replace(/\s/g, '');
+}
+
+function noSpace(x) {
+  return x.replaceAll(' ', '');
+}
+
+const noSpace = x => x.replace(/ /g, "");
+
+function noSpace(x){
+  var result = ""
+  for(var index = 0; index < x.length; index++){
+    if(x[index] !== " "){
+      result += x[index];
+    }
+  }
+  return result;
+}
+
+*/

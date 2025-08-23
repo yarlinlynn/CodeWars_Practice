ee666c9873777fc0b4f3d2a3cdce01abff0f8fa5
Yarlin <yarlinlynn@gmail.com>
2025-08-23 12:27:09 +0200

    solution for doubling a number

---
+++ javascript/8kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,22 @@
+/*
+Instructions:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// Solution:
+const doubleInteger = (i) => i * 2;
+
+/*
+Best rated solutions:
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i+i;
+}
+
+*/

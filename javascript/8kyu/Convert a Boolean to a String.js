ba740bf9bf959cfdbaaa66a60378af55ab678e3b
Yarlin <yarlinlynn@gmail.com>
2025-08-23 12:32:58 +0200

    solution for returning the string representation of a boolean

---
+++ javascript/8kyu/Convert a Boolean to a String.js
@@ -0,0 +1,34 @@
+/*
+Instructions:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+// Solution:
+function booleanToString(b){
+  return String(b)
+}
+
+/*
+Best rated solutions:
+
+function booleanToString(b){
+  return b.toString();
+}
+
+function booleanToString(b){
+  return b ? 'true' : 'false';
+}
+
+function booleanToString(b){
+  return `${b}`
+}
+
+const booleanToString = b => b ? 'true' : 'false';
+
+function booleanToString(b){
+  return b+'';
+}
+
+*/

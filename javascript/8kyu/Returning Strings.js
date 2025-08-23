7f1b2dba1e4831f1a0810653258e983dce959bcb
Yarlin <yarlinlynn@gmail.com>
2025-08-23 12:41:29 +0200

    solution for returning string using given name

---
+++ javascript/8kyu/Returning Strings.js
@@ -0,0 +1,22 @@
+/*
+Instructions:
+Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+// Solution:
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+/*
+Best rated solutions:
+
+const greet = name => `Hello, ${name} how are you doing today?`;
+
+function greet(name) {
+  return "Hello, " + name + " how are you doing today?";
+}
+
+*/

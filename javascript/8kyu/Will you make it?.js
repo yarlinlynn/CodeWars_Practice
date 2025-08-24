0886702ec8bdb662a7b13073e745da32c16ea521
Yarlin <yarlinlynn@gmail.com>
2025-08-24 17:12:24 +0200

    calculate distance and compare distance to still travel

---
+++ javascript/8kyu/Will you make it?.js
@@ -0,0 +1,29 @@
+/*
+Instructions:
+You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not.
+*/
+
+// Solution:
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  let distance = mpg * fuelLeft
+  return distance >= distanceToPump
+};
+
+/*
+Best rated solutions:
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump/mpg <= fuelLeft
+};
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return (mpg * fuelLeft) >= distanceToPump;
+};
+
+*/

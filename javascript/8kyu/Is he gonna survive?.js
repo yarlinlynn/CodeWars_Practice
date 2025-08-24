b4800f9ea34dc4cd555439345e889ab9658c923f
Yarlin <yarlinlynn@gmail.com>
2025-08-24 16:56:01 +0200

    return true or false if survives

---
+++ javascript/8kyu/Is he gonna survive?.js
@@ -0,0 +1,38 @@
+/*
+Instructions:
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+*/
+
+// Solution:
+function hero(bullets, dragons){
+  //calculate bullets required
+  let ammoNeeded = dragons * 2
+  //compare if its true or false
+  return bullets >= ammoNeeded
+}
+
+/*
+Best rated solutions:
+
+function hero(bullets, dragons){
+  return bullets >= dragons * 2
+}
+
+function hero(bullets, dragons){
+    return (bullets / 2 >= dragons) ? true : false;
+}
+
+const hero = (bullets, dragons) => dragons * 2 <= bullets;
+
+hero = (bullets, dragons) =>{
+if (bullets/2 >= dragons) {
+  return true;
+}
+else {
+  return false;
+}
+}
+
+*/

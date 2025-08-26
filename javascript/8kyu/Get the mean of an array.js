0610bec5f218d0488bcc32650d8e62571dacf476
Yarlin <yarlinlynn@gmail.com>
2025-08-26 10:34:11 +0200

    return sum of array rounded down

---
+++ javascript/8kyu/Get the mean of an array.js
@@ -0,0 +1,27 @@
+/*
+Instructions:
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+
+// Solution:
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  let sum = marks.reduce( (acc, value) => acc + value, 0)
+  let average = sum / marks.length
+  return Math.floor(average);
+}
+
+/*
+Best rated solutions:
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+}
+
+const getAverage = marks => Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+
+*/

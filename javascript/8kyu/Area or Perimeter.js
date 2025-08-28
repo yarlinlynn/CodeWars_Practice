2227a719e7612e06aab2c5ce7b165b06234c8537
Yarlin <yarlinlynn@gmail.com>
2025-08-28 12:23:08 +0200

    Return the area or perimeter

---
+++ javascript/8kyu/Area or Perimeter.js
@@ -0,0 +1,31 @@
+/*
+Instructions:
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+
+// Solution:
+//area = l * w if both are equal or perimater = 2 * (l + w)
+const areaOrPerimeter = (l, w) => (l === w) ? l * w : 2 * (l + w)
+
+/*
+Best rated solutions:
+
+const areaOrPerimeter = function(l , w) {
+  let area = l * w;
+  let perimeter = (l + w) * 2;
+  
+  return l === w ? area : perimeter;
+};
+
+const areaOrPerimeter = function(l , w) {
+  return l == w ? l*w : 2*(l + w)
+};
+
+*/

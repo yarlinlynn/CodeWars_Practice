a8a190c21b59edbeb3e0e6af0c5da06a0b68d8d9
Yarlin <yarlinlynn@gmail.com>
2025-09-02 14:40:34 +0200

    Remove ! from the end of a string & return new string

---
+++ javascript/8kyu/Exclamation marks series #1: Remove an exclamation mark from the end of string.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+*/
+
+// Solution:
+const remove = string => string.replace(/!$/, '');
+
+/*
+Best rated solutions:
+
+function remove (string) {
+    return string.replace(/!$/, '');
+}
+
+function remove(s) {
+  return s.endsWith('!') ? s.slice(0, -1) : s;
+}
+
+function remove(s){
+  return s[s.length - 1] == '!' ? s.slice(0, -1) : s;
+}
+
+*/

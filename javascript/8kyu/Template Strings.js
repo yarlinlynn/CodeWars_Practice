057188c3cb1e77743676a58c564f34e7def63369
Yarlin <yarlinlynn@gmail.com>
2025-09-02 15:08:40 +0200

    Return string using template strings incorportaing the use of the word are

---
+++ javascript/8kyu/Template Strings.js
@@ -0,0 +1,20 @@
+/*
+Instructions:
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+*/
+
+// Solution:
+var templateStrings = function(noun, adjective) {
+  return `${noun} are ${adjective}`
+}
+
+/*
+Best rated solutions:
+
+let TempleStrings = (obj, feature) => `${obj} are ${feature}`;
+
+*/

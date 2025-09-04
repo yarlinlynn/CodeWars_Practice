5c4f217f8a44c76a132cbb3a1a5e38b32e04719e
Yarlin <yarlinlynn@gmail.com>
2025-09-04 09:19:09 +0200

    Add item to array

---
+++ javascript/8kyu/ Basic Training: Add item to an Array.js
@@ -0,0 +1,19 @@
+/*
+Instructions:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+// Solution:
+websites.push("codewars")
+
+/*
+Best rated solutions:
+
+websites[0]="codewars";
+
+*/

00dae6e7ed602ba27cb5919981ea0bc74bbe29c4
Yarlin <yarlinlynn@gmail.com>
2025-09-06 14:21:15 +0200

    Return a reversed sentence

---
+++ javascript/8kyu/Reversed Words.js
@@ -0,0 +1,25 @@
+/*
+Instructions:
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+*/
+
+// Solution:
+const reverseWords = str => str.split(" ").reverse().join(" ")
+
+/*
+Best rated solutions:
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" "); // reverse those words
+}
+
+function reverseWords(str){
+  return str.trim().split(' ').reverse().join(' '); // reverse those words
+}
+
+*/

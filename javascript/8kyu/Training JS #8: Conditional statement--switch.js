b2de47ad32b1d1dc8e9fd547dd1482b8e2f89ed7
Yarlin <yarlinlynn@gmail.com>
2025-09-08 17:19:38 +0200

    Replace if-statement using switch statement to find how many days are in a month

---
+++ javascript/8kyu/Training JS #8: Conditional statement--switch.js
@@ -0,0 +1,64 @@
+/*
+Instructions:
+Complete the function howManydays. It accepts 1 parameter month, which means the month of the year. Different months have a different number of days as shown in the table below. Return the number of days that are in month. There is no need for input validation: month will always be greater than 0 and less than or equal to 12.
+
++---------------+-------------+
+|    month      |    days     |
++---------------+-------------+
+|1,3,5,7,8,10,12|     31      |
++---------------+-------------+
+|4,6,9,11       |     30      |
++---------------+-------------+
+|2              |     28      |  (Do not consider the leap year)
++---------------+-------------+
+*/
+
+function howManydays(month){
+  var days;
+  switch (month){
+    case 1 :
+    case 3 :
+    case 5 :
+    case 7 :
+    case 8 :
+    case 10 :
+    case 12 :
+      days = 31
+    break
+    case 4 :
+    case 6 :
+    case 9 :
+    case 11 :
+      days = 30
+    break
+    case 2: 
+      days = 28
+      break
+
+  }
+  return days;
+}
+
+/*
+Best rated solutions:
+
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
+
+function howManydays(month){
+  switch ( month ){
+    case 2 : return 28
+    case 4: case 6: case 9: case 11: return 30
+    default: return 31
+  }
+}
+
+*/

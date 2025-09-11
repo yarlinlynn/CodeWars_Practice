edbb4c966192b6d63668a6aacf7b773a4481a097
Yarlin <yarlinlynn@gmail.com>
2025-09-11 17:50:51 +0200

    Use break for loop checking an if statement

---
+++ javascript/8kyu/Training JS #11: loop statement --break,continue.js
@@ -0,0 +1,39 @@
+/*
+Instructions:
+Coding in function grabDoll. function accept 1 parameter:dolls. it's a string array, a list of some dolls.
+
+You need traverse dolls by using for loop. If element is "Hello Kitty" or "Barbie doll", you should push it to a bag(bag is an array, I've defined in the function); if it's other strings, we should use continue skip it.
+
+When the bag has three element, bag is full. You should use break jump out the loop; If bag is not full, you should traverse dolls until the last element.
+
+Return the bag after for loop finished.
+
+You should use for, break and continue in your code. otherwise, your solution may not pass this kata.
+*/
+
+// Solution:
+function grabDoll(dolls){
+  var bag=[];
+  //coding here
+  for (let i = 0; i < dolls.length; i++) {
+    if(bag.length === 3) {
+      break;
+    } else if (dolls[i] == 'Hello Kitty' || dolls[i] == 'Barbie doll') {
+      bag.push(dolls[i])
+    }
+  }
+  return bag;
+}
+
+/*
+Best rated solutions:
+
+function check(a,x){
+  return a.includes(x);
+};
+
+function check(a,x){
+  return a.indexOf(x) > -1 ? true : false;
+};
+
+*/

4abda8c14b091d38bf07f770f6bbfa5a7a867d12
Yarlin <yarlinlynn@gmail.com>
2025-09-11 18:03:36 +0200

    Return the square of a number using a function

---
+++ javascript/8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,19 @@
+/*
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// Solution:
+const square = n => n ** 2;
+
+/*
+Best rated solutions:
+
+function square(num){
+  var num = Math.pow(num, 2);
+  return num;
+};
+
+const square = a => a * a;
+
+*/

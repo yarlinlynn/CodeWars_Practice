691e3bd4517122a1e52564aa57f82f9a4bd3ecf4
Yarlin <yarlinlynn@gmail.com>
2025-09-16 07:38:44 +0200

    Debug the sayHello function to return Hello + name

---
+++ javascript/8kyu/Grasshopper - Debug sayHello.js
@@ -0,0 +1,23 @@
+/*
+Instructions:
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+// Solution:
+const sayHello = name => `Hello, ${name}`
+
+/*
+Best rated solutions:
+
+const sayHello = name => 'Hello, ' + name;
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
+
+*/

7654ed067cae50aaec189ccf0f105d7059925347
Yarlin <yarlinlynn@gmail.com>
2025-09-25 21:30:56 +0200

    Produces ouput according to the received inputed string

---
+++ javascript/8kyu/L1: Bartender, drinks.js
@@ -0,0 +1,82 @@
+/*
+Instructions:
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+*/
+
+// Solution:
+function getDrinkByProfession(param){
+ let inputString = param.toLowerCase();
+  if (inputString === "jabroni") {
+    return "Patron Tequila";
+  } else if (inputString === "school counselor") {
+    return "Anything with Alcohol";
+  } else if (inputString === "programmer") {
+    return "Hipster Craft Beer";
+  } else if (inputString === "bike gang member") {
+    return "Moonshine";
+  } else if (inputString === "politician") {
+    return "Your tax dollars"
+  } else if (inputString === "rapper") {
+    return "Cristal"
+  } else {
+    return "Beer"
+  }
+
+}
+
+/*
+Best rated solutions:
+
+function getDrinkByProfession(param) {
+  param = param.toLowerCase();
+  
+  switch(param) {
+    case "jabroni": return "Patron Tequila";
+    case "school counselor": return "Anything with Alcohol";
+    case "programmer": return "Hipster Craft Beer";
+    case "bike gang member": return "Moonshine";
+    case "politician": return	"Your tax dollars";
+    case "rapper": return "Cristal";
+    default: return "Beer";
+  }
+}
+
+const drinks = {
+  "jabroni": "Patron Tequila",
+  "school counselor": "Anything with Alcohol",
+  "programmer": "Hipster Craft Beer",
+  "bike gang member": "Moonshine",
+  "politician": "Your tax dollars",
+  "rapper": "Cristal"
+}
+
+const getDrinkByProfession = profession => drinks[profession.toLowerCase()] || "Beer"
+
+function getDrinkByProfession(param) {
+  const drinks = {
+    "jabroni": "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    "programmer": "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    "politician": "Your tax dollars",
+    "rapper": "Cristal"
+  };
+
+  return drinks[param.toLowerCase()] || "Beer";
+}
+
+*/

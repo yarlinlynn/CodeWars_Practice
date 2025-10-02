11d5ccc3f9f1e41f2d8802b4f8dba51917a253e9
Yarlin <yarlinlynn@gmail.com>
2025-10-02 20:36:43 +0200

    Debug function syntax mistakes

---
+++ javascript/8kyu/Grasshopper - Function syntax debugging.js
@@ -0,0 +1,20 @@
+/*
+Instructions:
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main [verb, noun]
+  return verb + noun
+}
+*/
+
+// Solution:
+const main = (verb, noun) => verb + noun;
+
+/*
+Best rated solutions:
+
+function main (verb, noun) {
+   return verb + noun
+}
+
+*/

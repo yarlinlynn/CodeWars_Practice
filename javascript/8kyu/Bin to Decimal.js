01a41215a60ac99ea23d0aeed41874838e0db5ca
Yarlin <yarlinlynn@gmail.com>
2025-10-08 20:29:30 +0200

    Convert binary number to decimal number

---
+++ javascript/8kyu/Bin to Decimal.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+// Solution:
+const binToDec = bin => parseInt(bin, 2)
+
+/*
+Best rated solutions:
+
+function binToDec(bin) {
+  return parseInt(bin, 2)
+}
+
+*/

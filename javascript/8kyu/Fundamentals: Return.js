55ecfaf048ad40586e90996bb033e356fd10a3ae
Yarlin <yarlinlynn@gmail.com>
2025-10-10 20:33:39 +0200

    Reurn sum, difference, modulus, product and exponent

---
+++ javascript/8kyu/Fundamentals: Return.js
@@ -0,0 +1,57 @@
+/*
+Instructions:
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+*/
+
+// Solution:
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return Math.pow(a, b)
+}
+    
+function subt(a,b){
+    return a - b
+}
+
+/*
+Best rated solutions:
+
+const add = (a, b) => a + b;
+const subt = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+
+*/

5d816a5488a76a923033dccf6739d384e6b64721
Yarlin <yarlinlynn@gmail.com>
2025-10-20 08:24:37 +0200

    Return given number multiplied by 50, adding 6 or Error if given value isnt a number

---
+++ javascript/8kyu/Super Duper Easy.js
@@ -0,0 +1,26 @@
+/*
+Instructions:
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
+
+// Solution:
+const problem = x => typeof x === 'number' ? (x * 50) + 6 : "Error";
+
+/*
+Best rated solutions:
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+
+function problem(x){
+  if (typeof x === 'number') {
+    return (x * 50) + 6
+  } else {
+    return "Error"
+  }
+}
+
+function problem(x){
+  return typeof x == 'string' ? 'Error' : (x * 50 + 6);
+}
+
+*/

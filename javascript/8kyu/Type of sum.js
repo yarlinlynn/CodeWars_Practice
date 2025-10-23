92557c7b04db4e93c032e42bc64ba4605dbc7c32
Yarlin <yarlinlynn@gmail.com>
2025-10-23 11:27:20 +0200

    Return the type of the sum of two arguments

---
+++ javascript/8kyu/Type of sum.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+Return the type of the sum of the two arguments
+*/
+
+// Solution:
+const typeOfSum = (a, b) => typeof(a + b);
+
+/*
+Best rated solutions:
+
+function typeOfSum(a, b) {
+  return typeof(a + b);
+}
+
+*/

592775a9d39c7a8333350e4ee0afc21da2c5810a
Yarlin <yarlinlynn@gmail.com>
2025-11-19 16:27:55 +0200

    Return true if given values are of the same character regardless of data type

---
+++ javascript/8kyu/Simple Comparison.js
@@ -0,0 +1,22 @@
+/*
+Instructions:
+Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number()
+*/
+
+// Solution:
+const add = (a, b) => a == b ? true : false;
+
+/*
+Best rated solutions:
+
+function add(a, b){
+	return a == b 
+}
+
+function add(a, b){
+	return +a == +b;
+}
+
+*/

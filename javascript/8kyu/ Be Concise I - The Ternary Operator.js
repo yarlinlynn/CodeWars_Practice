5cfda6ff146ae58384af6efc6649129379d78215
Yarlin <yarlinlynn@gmail.com>
2025-11-27 14:26:51 +0200

    Refactor if statement using ternary operator

---
+++ javascript/8kyu/ Be Concise I - The Ternary Operator.js
@@ -0,0 +1,27 @@
+/*
+Instructions:
+You are given a function that takes a parameter age (which will always be a positive integer) and does the following:
+
+If the age is 12 or lower, it return "You're a(n) kid"
+If the age is anything between 13 and 17 (inclusive), it return "You're a(n) teenager"
+If the age is anything between 18 and 64 (inclusive), it return "You're a(n) adult"
+If the age is 65 or above, it return "You're a(n) elderly"
+Your task is to shorten the code as much as possible. Note that submitting the given code will not work because there is a character limit of 137.
+
+function describeAge(age) {
+  if (age <= 12) {
+    return "You're a(n) kid";
+  } else if (age >= 13 && age <= 17) {
+    return "You're a(n) teenager";
+  } else if (age >= 18 && age <= 64) {
+    return "You're a(n) adult";
+  } else {
+    return "You're a(n) elderly";
+  }
+}
+*/
+
+// Solution:
+const describeAge = age =>
+  "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly");
+

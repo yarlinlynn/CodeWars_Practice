92a395557e368dc8b3df875dafda9056bea7b7e5
Yarlin <yarlinlynn@gmail.com>
2025-12-08 20:20:33 +0200

    Refactor object using oop including greet method

---
+++ javascript/7kyu/Refactored Greeting.js
@@ -0,0 +1,42 @@
+/*
+Instructions:
+The following code could use a bit of object-oriented artistry. While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. (Or, at least, something similar depending on your language)
+
+Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
+
+Here is how the final refactored code would be used:
+
+var joe = new Person('Joe');
+joe.greet('Kate'); // should return 'Hello Kate, my name is Joe'
+joe.name           // should == 'Joe'
+*/
+
+// Solution:
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  greet(name) {
+    return `Hello ${name}, my name is ${this.name}`;
+  }
+} 
+
+/*
+Best rated solutions:
+
+function Person(name){
+  this.name = name
+}
+Person.prototype.greet = function(yourName){
+  return "Hello " + yourName + ", my name is " + this.name
+}
+
+class Person {
+    constructor(name){
+        this.name = name;      
+        this.greet = (yourName) => `Hello ${yourName}, my name is ${this.name}`
+  }
+}
+
+*/

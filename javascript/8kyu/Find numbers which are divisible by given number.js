472581b0abd1bf7a2907f15e64175ccc3db1f382
Yarlin <yarlinlynn@gmail.com>
2025-12-13 18:52:46 +0200

    Return numbers that are divisible by given number

---
+++ javascript/8kyu/Find numbers which are divisible by given number.js
@@ -0,0 +1,29 @@
+/*
+Instructions:
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+*/
+
+// Solution:
+const divisibleBy = (numbers, divisor) => numbers.filter( num => num % divisor === 0)
+
+/*
+Best rated solutions:
+
+function divisibleBy(numbers, divisor){ 
+  return numbers.filter( num => num % divisor === 0);
+}
+
+function divisibleBy(numbers, divisor) {
+	var newArr = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] % divisor === 0) {
+			newArr.push(numbers[i]);
+		} 
+	}
+return newArr;
+}
+
+*/

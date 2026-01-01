df399167e62c68ed2706e2d226398dc48b59a8ab
Yarlin <yarlinlynn@gmail.com>
2026-01-01 23:14:21 +0200

    Retrun regular if ball type isn't given

---
+++ javascript/8kyu/Regular Ball Super Ball.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+// Solution:
+var Ball = function(ballType = "regular") {
+  this.ballType = ballType
+};
+
+/*
+Best rated solutions:
+
+var Ball = function(ballType) {
+  this.ballType = ballType || 'regular';
+};
+
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType;
+  }
+}
+
+*/

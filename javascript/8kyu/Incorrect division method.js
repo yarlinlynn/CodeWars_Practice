147ffabbf29effc068e241d624a4c5e1d6c5b41e
Yarlin <yarlinlynn@gmail.com>
2026-01-10 10:21:39 +0200

    Corrected divison method

---
+++ javascript/8kyu/Incorrect division method.js
@@ -0,0 +1,18 @@
+/*
+Instructions:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+const solve = (x, y) => x // y
+*/
+
+// Solution:
+const solve = (x, y) => x / y;
+
+/*
+Best rated solutions:
+
+function solve(x, y) {
+  return (y == 0) ? Infinity : (x / y);
+}
+
+*/

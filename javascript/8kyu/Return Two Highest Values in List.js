d38a8db6aaf0349b21ac238ef337b50ddb87fab6
Yarlin <yarlinlynn@gmail.com>
2026-01-18 20:53:25 +0200

    Return the two highest values in array

---
+++ javascript/8kyu/Return Two Highest Values in List.js
@@ -0,0 +1,24 @@
+/*
+Instructions:
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+*/
+
+// Solution:
+const twoHighest = (arr) => arr.length === 0 ? [] : [...new Set(arr)].sort( (a, b) => b - a).slice(0, 2)
+
+/*
+Best rated solutions:
+
+function twoHighest(arr) {
+  return [...new Set(arr)].sort((a, b) => b - a).slice(0, 2)
+}
+
+*/

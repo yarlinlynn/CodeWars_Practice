32a1a8f514fbe9582341f4f8766badf90a1bf30d
Yarlin <yarlinlynn@gmail.com>
2026-01-28 19:57:25 +0200

    Return the number 5 without using 0123456789*+-/ characters

---
+++ javascript/8kyu/5 without numbers !!.js
@@ -0,0 +1,23 @@
+/*
+Instructions:
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+// Solution:
+function unusualFive() {
+  return "five!".length;
+}
+
+/*
+Best rated solutions:
+
+function unusualFive() {
+    var str = ["one", "two", "three", "four", "five"]
+    return str.length;
+}
+
+*/

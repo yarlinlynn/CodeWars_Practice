dcd6797f3fabdd3f866f631cfa8b0ec473de36cf
Yarlin <yarlinlynn@gmail.com>
2026-02-04 21:00:34 +0200

    Return corresponding ASCII character for given value

---
+++ javascript/8kyu/get character from ASCII Value.js
@@ -0,0 +1,22 @@
+/*
+Instructions:
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+// Solution:
+const getChar = (c) => String.fromCharCode(c)
+
+/*
+Best rated solutions:
+
+function getChar(c) {
+  return String.fromCharCode(c)
+}
+
+*/

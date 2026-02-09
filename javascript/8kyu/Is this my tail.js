e2dfc99657ebcc330afc4a016567e2b4688c5b31
Yarlin <yarlinlynn@gmail.com>
2026-02-09 16:56:29 +0200

    Fix function that checks whether the animals have the correct tails

---
+++ javascript/8kyu/Is this my tail.js
@@ -0,0 +1,42 @@
+/*
+Instructions:
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+The arguments will always be non empty strings, and normal letters.
+
+function correctTail(bod, tail ;
+  
+  sub = body.substr(bodylength-(tail.length)
+  
+  if sub = tail) ;
+    return true
+  }
+  else 
+    return false
+*/
+
+// Solution:
+  
+const correctTail = (bod, tail) => bod.endsWith(tail)
+
+/*
+Best rated solutions:
+
+function correctTail(bod, tail) {
+  return bod[bod.length-1] === tail
+}
+
+function correctTail(body, tail) {
+  
+  var sub = body.substr(body.length-(tail.length));
+  
+  if (sub === tail) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+*/

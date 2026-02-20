60839d6b2040e6c3b2d4dc1f6c8aba2f3a109365
Yarlin <yarlinlynn@gmail.com>
2026-02-20 10:01:24 +0200

    Checked if item belonged to the list

---
+++ javascript/8kyu/Enumerable Magic - Does My List Include This.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+// Solution:
+const include = (arr, item) => arr.includes(item);
+
+/*
+Best rated solutions:
+
+function include(arr, item){
+  return arr.includes(item);
+}
+
+*/

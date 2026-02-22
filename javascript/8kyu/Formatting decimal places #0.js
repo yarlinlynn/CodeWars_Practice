0d4fb91ec39958c3659787ef38935e61ba251975
Yarlin <yarlinlynn@gmail.com>
2026-02-22 14:57:25 +0200

    Return given number with only two decimal places

---
+++ javascript/8kyu/Formatting decimal places #0.js
@@ -0,0 +1,21 @@
+/*
+Instructions:
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+-3.3424 is rounded -3.34
+
+*/
+
+// Solution:
+const twoDecimalPlaces = (n) => Number(n.toFixed(2))
+
+/*
+Best rated solutions:
+
+function twoDecimalPlaces(n) {
+  return parseFloat(n.toFixed(2));
+}
+
+*/

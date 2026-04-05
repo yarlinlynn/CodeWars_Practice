87a4f8356c7fce6bebe4e6ba59ad8711f7c255cc
Yarlin <yarlinlynn@gmail.com>
2026-04-05 23:22:25 +0200

    Return input number raised to the given power without using Math functions like Math.pow or **

---
+++ javascript/8kyu/Power.js
@@ -0,0 +1,26 @@
+/*
+Instructions:
+The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+
+Examples
+numberToPower(3, 2)  // -> 9 ( = 3 * 3 )
+numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10, 6) // -> 1000000
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+
+// Solution:
+function numberToPower(number, power){
+  let result = 1;
+  for(let i = 0; i < power; i++) {
+    result *= number;
+  }
+  return result;
+}
+
+/*
+Best rated solutions:
+
+const numberToPower = (number,power) => power > 0 ? number * numberToPower(number,power-1) : 1;
+
+*/

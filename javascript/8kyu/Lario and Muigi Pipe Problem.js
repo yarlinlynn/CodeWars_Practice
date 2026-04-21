bddbe669711a9b87b657b40d4cc6c3e5dde69f9a
Yarlin <yarlinlynn@gmail.com>
2026-04-21 10:46:22 +0200

    Returned new array of values incremented by 1 for each index from min to max values

---
+++ javascript/8kyu/Lario and Muigi Pipe Problem.js
@@ -0,0 +1,33 @@
+/*
+Instructions:
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+Task
+Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+Example
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+*/
+
+// Solution:
+function pipeFix(numbers){
+  let firstNum = numbers[0];
+  let lastNum = numbers[numbers.length - 1];
+  let newArr = [];
+  
+  for(let i = firstNum; i <= lastNum; i++) {
+    newArr.push(i)
+  }
+  return newArr;
+}
+
+/*
+Best rated solutions:
+
+let pipeFix = nums => Array.from({ length: nums.pop() - nums[0] + 1 }, (v, i) => i + nums[0]);
+
+*/

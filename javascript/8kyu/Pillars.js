1e738a3712acb5d13b404c073fa2d99c24449d99
Yarlin <yarlinlynn@gmail.com>
2026-05-05 17:49:57 +0200

    Returned the distance bwteen first and last pillar in cm

---
+++ javascript/8kyu/Pillars.js
@@ -0,0 +1,34 @@
+/*
+Instructions:
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+*/
+
+// Solution:
+function pillars(numPill, dist, width) {
+  if (numPill < 2) return 0;
+  return ( (numPill - 1) * dist * 100) + ( (numPill - 2) * width);
+}
+
+/*
+Best rated solutions:
+
+function pillars(num_pill, dist, width) {
+  return num_pill > 1 ? (num_pill - 1) * dist * 100 + (num_pill - 2) * width : 0;
+}
+
+function pillars(num_pill, dist, width) {
+    if (num_pill === 1) {
+        return 0;
+    }else{
+        let distance = dist * ((num_pill -1) * 100 );
+        let pillars = num_pill - 2;
+        return distance + (width * pillars);
+    }  
+}
+
+*/

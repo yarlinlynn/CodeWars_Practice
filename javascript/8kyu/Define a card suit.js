7898ba160aa03d5a4441aa29f4e52a619af4e7cd
Yarlin <yarlinlynn@gmail.com>
2026-06-01 14:55:19 +0200

    Returned the card symbol from given input

---
+++ javascript/8kyu/Define a card suit.js
@@ -0,0 +1,45 @@
+/*
+Instructions:
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+// Solution:
+function defineSuit(card) {
+  const symbol = card.charAt(card.length - 1);
+  
+  switch(symbol) {
+      case '♣': return 'clubs';
+      case '♦': return 'diamonds'
+      case '♥': return 'hearts';
+      case '♠': return 'spades';
+  }
+}
+
+/*
+Best rated solutions:
+
+function defineSuit(card) {
+  const s = {
+    "♣": "clubs",
+    "♠": "spades",
+    "♦": "diamonds",
+    "♥": "hearts"
+  }
+  return s[card.charAt(card.length - 1)]
+}
+
+function defineSuit(card) {
+    if(card.includes('♥')) return 'hearts'
+    if(card.includes('♦')) return 'diamonds'
+    if(card.includes('♣')) return 'clubs'
+    if(card.includes('♠')) return 'spades' 
+}
+
+*/

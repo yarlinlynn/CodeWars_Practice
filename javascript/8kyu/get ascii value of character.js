69a2c9bc4d040e587ed201d67531c89d2307e4d7
Yarlin <yarlinlynn@gmail.com>
2026-06-03 18:43:03 +0200

    Return ASCII value of given character

---
+++ javascript/8kyu/get ascii value of character.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+Get ASCII value of a character.
+*/
+
+// Solution:
+const getASCII = c => c.charCodeAt(0);
+
+/*
+Best rated solutions:
+
+function getASCII(c){
+    return c.charCodeAt();
+}
+
+*/

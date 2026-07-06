99e4c713a94f29ddef5788487ade72d29cca6745
Yarlin <yarlinlynn@gmail.com>
2026-07-06 20:11:44 +0200

    Returns ascii characters converted from array of numbers

---
+++ javascript/8kyu/Take an Arrow to the knee, Functionally.js
@@ -0,0 +1,22 @@
+/*
+Instructions:
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+*/
+
+// Solution:
+const arrowFunc = (arr) => arr.map(code => String.fromCharCode(code)).join('');
+
+/*
+Best rated solutions:
+
+var ArrowFunc = function(arr) {
+  return arr.map( x => String.fromCharCode(x) ).join(''); 
+}
+
+const ArrowFunc = (arr) => String.fromCharCode(...arr);
+
+*/

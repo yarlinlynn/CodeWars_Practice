75af7c89a257c0169a03e5cb4e15adc0f6d73bcb
Yarlin <yarlinlynn@gmail.com>
2026-08-11 20:24:29 +0200

    Returned myFirstKata function without errors

---
+++ javascript/8kyu/Fix the Bugs (Syntax) - My First Kata.js
@@ -0,0 +1,36 @@
+/*
+Instructions:
+Hello, this is my first Kata so forgive me if it is of poor quality.
+
+In this Kata you should fix/create a program that returns the following values:
+
+false/False if either a or b (or both) are not numbers
+a % b plus b % a if both arguments are numbers
+You may assume the following:
+
+If a and b are both numbers, neither of a or b will be 0.
+
+func myFirstKata(a b) {
+  if {typeof(a) !! "number" or typeof(b) !! "number"} (
+    false;
+  ) else (
+    a % b ++ b % a;
+  )
+}
+*/
+
+// Solution:
+const myFirstKata = (a, b) => typeof a !== "number" || typeof b !== "number" ? false : a % b + b % a;
+
+/*
+Best rated solutions:
+
+function myFirstKata(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return false;
+  } else {
+    return (a % b) + (b % a);
+  }
+}
+
+*/
